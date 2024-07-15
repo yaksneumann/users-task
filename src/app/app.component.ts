543320a31978a7834d28fb3549e6d4de9257c872
yaksneumann <whywhyn@gmail.com>
2024-07-15 07:11:26 +0300

    added dearch c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { UsersComponent } from './users/users.component';
 import { NavComponent } from './nav/nav.component';
-import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
     RouterOutlet,
-    NavComponent,
-    HttpClientModule
+    NavComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
+import { ApiService } from '../api.service';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
+  searchNumber: number | null = null;
+  searchResult: string = '';
 
+  constructor(private dialog: MatDialog, private api: ApiService) {}
+
+  onValueChange(value: string) {
+    this.searchNumber = value ? Number(value) : null;
+  }
+
+  onSearch() {
+    console.log(this.searchNumber);
+    if (this.searchNumber !== null)
+      this.searchResult = `You searched for ${this.searchNumber}`;
+    const userId = this.searchNumber as number;
+    this.api.getTasksByUser(userId).subscribe({
+      next: (data) => {
+        const userTasks = data;
+        this.dialog.open(ModalComponent, {
+          data: { name: this.searchNumber, tasks: userTasks },
+        });
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      },
+    });
+  }
 }
